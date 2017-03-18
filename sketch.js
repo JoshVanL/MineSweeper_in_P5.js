723cee970dc7e74dc2182251da67277173829fe8
jv15626 <jv15626@my.bristol.ac.uk>
2017-03-18 01:48:53 +0000

    Can click on squares, working on searching

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ var sizexy = 20;
 var bombs = 40;
 var board;
 var squares = [];
+var gameover = 0;
 
 function setup() {
     setFrameRate(60);
@@ -18,14 +19,14 @@ function setup() {
         squares[i] = [];
         for (var j=0; j<m; j++) {
             squares[i][j] = new Square(x, y, 20, sizexy);
-            x += sizexy;
+            y += sizexy;
         }
-        x = 0;
-        y += sizexy;
+        y = 0;
+        x += sizexy;
     }
 
     for(var i=0; i<bombs; i++) {
-        squares[ Math.floor(bombsi[i]/m) ][ bombsi[i] % m ].type = 1; // Set as bombs
+        squares[Math.floor(bombsi[i]/m) ][ bombsi[i] % m ].setAsBomb(); // Set as bombs
     }
 
 }
@@ -41,6 +42,21 @@ function draw() {
 
 
 function mousePressed() {
+    // Find square clicked
+    if(mouseX < m*sizexy && mouseY < m*sizexy) {
+        i = Math.floor(mouseX/sizexy);
+        j = Math.floor(mouseY/sizexy)
+        gameover = squares[i][j].clicked();
+
+        if(!gameover) {
+            search(i, j);
+        }
+
+    }
+}
+
+
+function search(i, j) {
 
 }
 
--- square.js
+++ square.js
@@ -1,8 +1,10 @@
 function Square(x, y, cl, sizexy) {
+
     this.xpos = x;
     this.ypos = y;
     this.c = cl;
-    this.type = 0;  // Normal
+    this.isBomb = 0;  // Normal
+    this.seen = 0;
 
     this.display = function() {
         fill(this.c);
@@ -14,5 +16,22 @@ function Square(x, y, cl, sizexy) {
         fill(this.c);
         rectMode(CORNER);
         rect(this.xpos, this.ypos, sizexy, sizexy);
+    }
+
+    this.setAsBomb = function() {
+        this.isBomb = 1;
+    }
+
+    this.clicked = function() {
+        console.log(this.xpos, this.ypos);
+        this.seen = 1;
+        if(!this.isBomb){
+            this.c = 200;
+            return 0;
+
+        } else {
+            this.c = 100;
+            return 1;
+        }
     }
 }
